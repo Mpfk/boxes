8621f13119d9c4ce663c0516f826a3bf9169de31
Matt Lydon <489306+Mpfk@users.noreply.github.com>
2024-07-28 14:51:01 -0400

    chore: Add quantity and note fields to ItemForm.vue

--- src/components/ItemForm.vue
+++ src/components/ItemForm.vue
@@ -28,6 +28,14 @@
       <input id="itemName" class="form-control" v-model="itemName" @input="updateItemName" type="text" />
       <div id="nameHelp" class="form-text">Required</div>
     </div>
+    <div class="mb-3">
+      <label for="quantity" class="form-label">Quantity</label>
+      <input id="quantity" class="form-control" type="number" />
+    </div>
+    <div class="mb-3">
+      <label for="note" class="form-label">Note</label>
+      <textarea id="note" class="form-control" rows="3"></textarea>
+    </div>
   </div>
 </template>
 

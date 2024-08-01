83b6df33b2ffa959ee19b3056b188a68f95099e4
Matt Lydon <489306+Mpfk@users.noreply.github.com>
2024-08-01 02:11:48 -0400

    optimize: lazy loading

--- src/router.ts
+++ src/router.ts
@@ -1,25 +1,50 @@
 // Imports
 import { createRouter, createWebHistory } from 'vue-router';
-import Options from './components/Options.vue';
-import Boxes from './components/Boxes.vue';
-import BoxNew from './components/BoxNew.vue';
-import BoxDetails from './components/BoxDetails.vue';
-import BoxEdit from './components/BoxEdit.vue';
-import ItemNew from './components/ItemNew.vue';
-import ItemEdit from './components/ItemEdit.vue';
-import NotFound from './components/NotFound.vue';
+const Boxes = () => import('./components/Boxes.vue');
+const NotFound = () => import('./components/NotFound.vue');
 
-// Vars
+// Routes
 const routes = [
-  { path: '/', component: Boxes },
-  { path: '/options', component: Options },
-  { path: '/box/new', component: BoxNew, props: true },
-  { path: '/box/:boxID', component: BoxDetails, props: true },
-  { path: '/box/:boxID/edit', component: BoxEdit, props: true },
-  { path: '/box/:boxID/fill', component: ItemNew, props: true },
-  { path: '/box/:boxID/item/:itemID/edit', component: ItemEdit, props: true },
-  { path: '/:notFound(.*)', component: NotFound },
-  { path: '/404', component: NotFound },
+  { path: '/', 
+    component: Boxes 
+  },
+  { 
+    path: '/options', 
+    component: () => import('./components/Options.vue') 
+  },
+  { 
+    path: '/box/new', 
+    component: () => import('./components/BoxNew.vue'), 
+    props: true 
+  },
+  { 
+    path: '/box/:boxID', 
+    component: () => import('./components/BoxDetails.vue'), 
+    props: true 
+  },
+  { 
+    path: '/box/:boxID/edit', 
+    component: () => import('./components/BoxEdit.vue'), 
+    props: true 
+  },
+  { 
+    path: '/box/:boxID/fill', 
+    component: () => import('./components/ItemNew.vue'), 
+    props: true 
+  },
+  { 
+    path: '/box/:boxID/item/:itemID/edit', 
+    component: () => import('./components/ItemEdit.vue'), 
+    props: true 
+  },
+  { 
+    path: '/:notFound(.*)', 
+    component: NotFound 
+  },
+  { 
+    path: '/404', 
+    component: NotFound 
+  },
 ];
 
 const router = createRouter({
